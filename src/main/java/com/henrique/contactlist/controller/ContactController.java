6e58b61830fb7fc62a7c8dbe9ddb23b68f1ecebd
Henrique Figueiredo <hs_figueiredo@hotmail.com>
2022-04-17 19:26:13 -0300

    Impl instrumentation for monitoring purpose

--- src/main/java/com/henrique/contactlist/controller/ContactController.java
+++ src/main/java/com/henrique/contactlist/controller/ContactController.java
@@ -4,6 +4,8 @@ import com.henrique.contactlist.dto.ContactDTO;
 import com.henrique.contactlist.dto.NewContactDTO;
 import com.henrique.contactlist.dto.UpdateContactDTO;
 import com.henrique.contactlist.service.IContactService;
+import io.micrometer.core.annotation.Counted;
+import io.micrometer.core.annotation.Timed;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -24,6 +26,7 @@ public class ContactController {
     }
 
     @PostMapping(path = "/api/v1/contacts")
+    @Timed(value = "contacts.create.time", description = "Time taken to create a contact")
     public ResponseEntity<ContactDTO> create(@RequestBody NewContactDTO newContactDTO) {
         logger.info(String.format("Creating new contact (%s)", newContactDTO.getName()));
         final ContactDTO result = this.contactService.create(newContactDTO);
@@ -31,12 +34,15 @@ public class ContactController {
     }
 
     @GetMapping("/api/v1/contacts")
+    @Timed(value = "contacts.list.time", description = "Time taken to list all contacts")
+    @Counted(value = "contacts.list.count")
     public List<ContactDTO> listAll() {
         logger.info("Performing contacts list.");
         return this.contactService.listAll();
     }
 
     @PutMapping(path = "/api/v1/contacts/{id}")
+    @Timed(value = "contacts.update.time", description = "Time taken to update a contact")
     public ResponseEntity update(@PathVariable(value = "id") final String uuid,
                                  @RequestBody final UpdateContactDTO contactDTO) {
         logger.info(String.format("Updating the contact (%s)", uuid));
@@ -45,6 +51,7 @@ public class ContactController {
     }
 
     @PatchMapping(path = "/api/v1/contacts/{id}/name/{name}")
+    @Timed(value = "contacts.update.name.time", description = "Time taken to update the name of a contact")
     public ResponseEntity updateNome(@PathVariable(value = "id") final String uuid,
                                      @PathVariable(value = "name") final String name) {
         logger.info(String.format("Updating contact name (%s) for contact (%s)", name, uuid));
@@ -53,6 +60,7 @@ public class ContactController {
     }
 
     @DeleteMapping("/api/v1/contacts/{id}")
+    @Timed(value = "contacts.delete.time", description = "Time taken to delete a contact")
     public ResponseEntity delete(@PathVariable(value = "id") final String uuid) {
         logger.info(String.format("Deleting the contact (%s)", uuid));
         this.contactService.delete(uuid);
